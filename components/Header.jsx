eba988e0be5d739dbadbaedd2f30b5bca4839330
kostiantynkovalchuk <176706484+kostiantynkovalchuk@users.noreply.github.com>
2025-05-25 23:50:21 +0200

    app name changed

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,10 @@
-import chefClaudeLogo from "../images/chef-claude-icon.png"
+import chefClaudeLogo from "../images/chef-claude-icon.png";
 
 export default function Header() {
-    return (
-        <header>
-            <img src={chefClaudeLogo}/>
-            <h1>Chef Claude</h1>
-        </header>
-    )
+  return (
+    <header>
+      <img src={chefClaudeLogo} />
+      <h1>Chef KOS</h1>
+    </header>
+  );
 }
